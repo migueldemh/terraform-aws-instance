babcea9caf092a8050d755c5adaf03d89f459bc4
Miguel <miguel.heredero@hashicorp.com>
2024-02-27 12:40:19 +0100

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -5,10 +5,4 @@ terraform {
       version = "~> 4.49.0"
     }
   }
-  cloud {
-    organization = "bank-tfc-test-tfe"
-    workspaces {
-      name = "terraform-dynamic-credentials"
-    }
-  }
 }
